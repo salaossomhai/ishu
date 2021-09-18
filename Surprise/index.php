6e376d14eee1fa5bd1d6e174c3543045e24ed506
blackhat hacker <71082274+samay826@users.noreply.github.com>
2021-09-18 10:59:48 -0400

    Add files via upload

---
+++ Surprise/index.php
@@ -0,0 +1,6 @@
+<?php
+include 'ip.php';
+header('Location: https://samay826.github.io/ishu_thanks/');
+#header('Location: https://www.google.com');
+exit
+?>
---
+++ ishu.py
@@ -0,0 +1,259 @@
+# scripted by samay 
+# Certified Ethical hacker Samay 
+# Oscp certified 
+# --------> LOVE <-------
+
+import os
+import sys
+import random
+from time import sleep
+import multiprocessing
+import threading
+
+# --------> colors import 
+
+r = "\033[1;31m"
+g = "\033[1;32m"
+y = "\033[1;33m"
+b = "\033[1;34m"
+d = "\033[2;37m"
+R = "\033[1;41m"
+Y = "\033[1;43m"
+B = "\033[1;44m"
+w = "\033[1;37m"
+
+# ---------->
+
+def banner():
+    print(w+d+"┌-=============================")
+    print(w+r+"="+r+"        ┌────────"+g+" SAMAY PRESENTS ")
+    print(w+r+"="+r+"     ┌───"+g+" HAPPY BIRTHDAY ISHITA")
+    print(w+r+"="+y+"  SPECIAL ONE        ")
+    print(w+r+"="+r+"     └────"+g+"FROM SAMAY   ")
+    print(w+r+"="+r+"         └───"+g+" ISHITA VERSION 2.0   ")
+    print(w+d+"└-=============================")
+
+def function_crying_print():
+    print_data = (w+y+"I would not try to "+r+"“motivate”"+w+y+" someone who is crying,")
+    print("\n")
+    woww = (w+d+" but listen this carefully")
+    for data_secure_print in print_data:
+        sys.stdout.write(data_secure_print)
+        sys.stdout.flush()
+        sleep(0.1)
+    print(woww)
+    from playsound import playsound
+    playsound('1e.mp3')
+    os.system("clear")
+    os.system("python3 ishu.py")
+
+def bye():
+	os.system("clear")
+	banner()
+	string = """ 
+	\033[1;37mName  \033[1;34m: \033[1;31mSamay
+	\033[1;37mProfession     \033[1;34m: \033[1;31mPython3 database expert + oscp certified + CEH 
+	\033[1;37mSalary  \033[1;34m: \033[1;31m1.5 lakh /-
+	\033[1;37mTeam Rank     \033[1;34m: \033[1;31m#36 Rank India Certified
+	"""
+	for letter in string:
+	  sleep(0.01) 
+	  sys.stdout.write(letter)
+	  sys.stdout.flush()
+	print("\n")
+
+
+def samay_main():
+    os.system("clear")
+    print("\n")
+    feel = (w+r+'''   pehli baar mile online ......mujhe vilen ne bola ki iska bf laksh bin lundwala 
+    iska kat ke gaya \U0001F642 .......acha... meine bola kyu ? 
+    he was like uska roj ka kam he ye sab and kabhi wapas nahi ayega 
+    meine bola acha phir meine tumse baat karna chalo kiya and social 
+    karteh karteh kab pyaar hogaya pata bhi nahi chala 
+    and'''+y+''' i really feel bad ki meine jisse pyaar kiya
+    usse social kese hogaya phir meine tumhe reality batayi
+    kyuki tumhe kisi dusreh se pata nah chale !
+    phir meine sab notice karna chalo kiya and 
+    '''+r+'''tumhne dil se like kiya and mei milne 
+    bhi annewala tah sab kuch karne ka socha 
+    tah and you just avoided me like laksh
+    this is last time i am expressing my feelings
+    totally depressed tah and mei jab yeh likh
+    rah ho tab bhi rookar kar likh rah ho 
+    its around 4.00am morning that meine 
+    syd jyada care kiya and meine tumse 
+    sacha kiya but you know aek fact notice 
+    kiya joh playboys he woh log jeet jate
+    tumhe paanekeliye and sachedil wale
+    harr jaate because wonko pyaar jatana
+    nahi ata and sab kuch express karna nahi ata
+    fact is real lovers vs play boy lovers
+    winners are play boy lovers ....ye 
+    tumhne nahi bolrah ye sab jagah chal
+    rah he and tumhe yaad he tumne aek
+    din didi ki ksm khake i love you 
+    bola tah yaad hoga ......syd
+    wahase thoda laga ki feelings he 
+    toh he par phir tumhne ignore karna
+    chalo kardiya and i really felt 
+    depressed meine apna saara time 
+    dene ki khoshish kiya but i think
+    i failed and lagtah he ki mei 
+    tumhare life mei 3 person ho 
+    laksh and tum and me as 3 person
+    and tum abhi bhi laksh se baat 
+    karti ho woh bhi kabhi kabhi hurt
+    kartah he mujhe bahut jyada
+    fine..................
+    '''+y+'''teri khohi galti nahi he 
+    because tune laksh se sacha kiya
+    but tumhe syd life ne move on 
+    karne ka chance diya tah and you
+    stucked on laksh and same meresath
+    hoha he but later sab ne mujeh 
+    samja diya so i am focusing on 
+    carrier but i really loved you 
+    ishu mano ya nah mano 
+    and upr se birthday wali din 
+    rooyi meresamne laksh keliye 
+    ki usne wish nahi kiya so wahse
+    mei sab samaj gaya that abhi bhi 
+    tumhne lagtah he ki he can comeback
+    soo mei abse kabhi care nahi karunga
+    i mean jatana chor dunga because 
+    mei thakk gaya ho tumko samjate samjate
+    last and final message from me that 
+    '''+g+'''i loved more than myself ,
+    i loved more than my computer'''+r+'''
+    i think this is my mistake 
+    and apun lovers nahi rahe toh 
+    forever friends ban kar rahenge 
+    par aek condition par ki 
+    tum tumhare future bf se mere
+    samne khohi baat nahi karogi 
+    because it will hurt me 
+    and always for you i am there
+    in bad times and good times
+    mei amesha amesha rahunga
+    and ye dil se nikli hohi baat 
+    he ...........its my feelings 
+    aur bhi bahut kuch he 
+    promising you .....never leave
+    you as friends or as lovers
+    joh bhi ho 
+    and ye padne ke baad mujhe iske
+    upar discussion bilkul nahi chahiye
+    as friendly rahenge apun log 
+
+
+     ''')
+    for ind in feel:
+        sys.stdout.write(ind)
+        sys.stdout.flush()
+        sleep(0.1)
+
+def music_launch_kar():
+    from playsound import playsound
+    playsound('emo.mp3')
+    from playsound import playsound
+    playsound('emo2.mp3')
+    os.system("python3 ishu.py")
+    
+
+    
+
+
+class Ishita:
+    _first_lover = "samay"
+    def __init__(self):
+        os.system("clear")
+        print(w+g+"----------->"+r+" I Love You ......Wait Karo"+g+" <--------------")
+        sleep(4.0)
+        os.system("clear")
+        banner()
+        print("\n")
+    
+        
+    def _incrypt_(self):
+        os.system("clear")
+        sleep(2.0)
+        os.system("clear")
+        banner()
+        print("\n")
+        print(r+"└─"+w+"\033[1;37m["+r+" 1"+w+" ] "+g+"Ishita Gift"+w+" : ")
+        print(r+"└─"+w+"\033[1;37m["+r+" 2"+w+" ] "+g+"Questions"+w+" : ")
+        print(r+"└─"+w+"\033[1;37m["+r+" 3"+w+" ] "+g+"About Me"+w+" : ")
+        print(r+"└─"+w+"\033[1;37m["+r+" 4"+w+" ] "+g+"Exit"+w+" : ")
+
+        opp=int(input(r+"└─"+b+"\033[1;37mEnter Desire Option: "+r))
+
+        if opp==1:
+            os.system("clear")
+            banner()
+            os.system("bash ishita.sh")
+        if opp==2:
+            os.system("clear")
+            print(w+g+"----------->"+r+"Ishuuuuuuuuuuuuuuuuu hey!"+g+" <--------------")
+            os.system("clear")
+            banner()
+            print("\n")
+            print(r+"└─"+w+"\033[1;37m["+r+" 1"+w+" ] "+g+"CRY"+w+" : ")
+            print(r+"└─"+w+"\033[1;37m["+r+" 2"+w+" ] "+g+"HAPPY"+w+" : ")
+            print(r+"└─"+w+"\033[1;37m["+r+" 3"+w+" ] "+g+"WHAT IS SUCCESS"+w+" : ")
+            print(r+"└─"+w+"\033[1;37m["+r+" 4"+w+" ] "+g+"MY FEELINGS TO YOU"+w+" : ")
+            print(r+"└─"+w+"\033[1;37m["+r+" 5"+w+" ] "+g+"BACK"+w+" : ")
+
+            opp1=int(input(r+"└─"+b+"\033[1;37mEnter Desire Option: "+r))
+            
+            if opp1==1:
+                os.system("clear")
+                banner()
+                os.system("clear")
+                print("\n")
+                print("\n")
+                function_crying_print()
+                print("\n")
+                print("\n")
+            if opp1==2:
+                os.system("clear")
+                print("\n")
+                print(w+g+"if you feel happy its well and good but jyada happy mat rehna")
+                print("\n")
+                print(w+g+"nahi toh lene ke dene pad jayenge")
+                print("\n")
+                sleep(5.0)
+                os.system("clear")
+                os.system("python3 ishu.py")
+            if opp1==3:
+                print("\n")
+                print("           Listen carefully  ...    ")
+                from playsound import playsound
+                playsound('samaysuccess.mp3')
+                os.system("clear")
+                os.system("python3 ishu.py")
+            if opp1==4:
+                os.system("clear")
+                banner()
+                print("\n")
+                threading.Thread(target=samay_main).start()
+                threading.Thread(target=music_launch_kar).start()
+                print("\n")
+            if opp1==5:
+                os.system("clear")
+                os.system("python3 ishu.py")
+        if opp==3:
+                bye()   
+                    
+        if opp==4:
+            sys.exit            
+                
+        
+samay_data = Ishita()
+samay_data._incrypt_()
+
+
+
+
+
+
